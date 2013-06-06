8a1e79d5384f06b64bb98216f46d32a76baf074d
Sean Yu <seansu4you87@gmail.com>
2013-06-05 18:32:51 -0700

    Better validation per input element on the send modal
    * recipient, amount, and description
    * description is a textarea
    * wrapping inputs in .control-group divs

--- app/scripts/controllers/send.js
+++ app/scripts/controllers/send.js
@@ -21,9 +21,46 @@ angular.module('evenlyApp')
       return /^\$?[0-9][0-9\,]*(\.\d{1,2})?$|^\$?[\.]([\d][\d]?)$/.test(value);
     };
 
-    $scope.isGte = function(value) {
+    $scope.isGte = function(value, min) {
+      min = min || 0.50;
       if (value === undefined) { return false; };
-      return value.replace(/[^0-9\.]/g,'') >= 0.50;
+      return value.replace(/[^0-9\.]/g,'') >= min;
     }
+
+    $scope.invalidRecipient = function() {
+      return !!$scope.form.recipient.$error.required;
+    }
+
+    $scope.invalidAmount = function() {
+      var gteError      = !!$scope.form.amount.$error.gte;
+      var currentError  = !!$scope.form.amount.$error.currency;
+      return gteError || currentError;
+    }
+
+    $scope.invalidDescription = function() {
+      return !!$scope.form.description.$error.required
+    }
+
+    $scope.invalidForm = function() {
+      var invalidAmount = $scope.invalidAmount();
+      var invalidDescription = $scope.invalidDescription();
+
+      return invalidAmount || invalidDescription;
+    }
+
+    $scope.classForRecipient = function() {
+      if ($scope.form.recipient.$viewValue === undefined) { return; };
+      return $scope.invalidRecipient() ? "error" : "success";
+    };
+
+    $scope.classForAmount = function() {
+      if ($scope.form.amount.$viewValue === undefined) { return; };
+      return $scope.invalidAmount() ? "error" : "success";
+    };
+
+    $scope.classForDescription = function() {
+      if ($scope.form.description.$viewValue === undefined) { return; };
+      return $scope.invalidDescription() ? "error" : "success";
+    };
 
   }]);
