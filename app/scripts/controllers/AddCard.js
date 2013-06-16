b1fa6271c5b2115832aa3c42f43b5a3ecb7ffd3f
Sean Yu <seansu4you87@gmail.com>
2013-06-15 17:23:00 -0700

    Adding validation for new card

--- app/scripts/controllers/AddCard.js
+++ app/scripts/controllers/AddCard.js
@@ -3,7 +3,24 @@
 angular.module('evenlyApp')
   .controller('AddCardCtrl', ['$scope', 'CreditCard', 'balanced', function ($scope, CreditCard, balanced) {
     $scope.addCard = function() {
-      console.log("adding card");
-      balanced.tokenizeCard($scope.card, null);
+      if ($scope.validForm()) {
+        console.log("adding card");
+        balanced.tokenizeCard($scope.card, null);
+      } else {
+        $scope.showErrors = true;
+      }
+    }
+
+    $scope.validForm = function() {
+      return !$scope.form.number.$error.cardNumber &&
+        !$scope.form.number.$error.required &&
+        !$scope.form.cvc.$error.cardCVC &&
+        !$scope.form.cvc.$error.required &&
+        !$scope.form.expiry.$error.cardExpiry &&
+        !$scope.form.expiry.$error.required;
+    }
+
+    $scope.classForButton = function() {
+      return $scope.validForm() ? "btn btn-primary" : "btn btn-primary disabled"
     }
   }]);
