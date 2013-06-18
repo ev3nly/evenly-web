a92073376773c8e1a6f99285b9a37638c93840ed
Sean Yu <seansu4you87@gmail.com>
2013-06-17 17:55:44 -0700

    Adding DepositCtrl

---
+++ app/scripts/controllers/deposit.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('evenlyApp')
+  .controller('DepositCtrl', ['$scope', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
