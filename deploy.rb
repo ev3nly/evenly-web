49217afcff389685fd7f95f72858393524fd71d7
Sean Yu <seansu4you87@gmail.com>
2013-06-24 19:50:44 -0700

    Rebuilding dist

--- deploy.rb
+++ deploy.rb
@@ -13,25 +13,25 @@ command :deploy do |c|
   c.summary = 'build distribution and push to github.io'
   c.action do |args, options|
 
-    if not File.exists?(current_path + '/ev3nly.github.io/.git')
-      speak_and_say 'cloning github pages url'
-      `git clone #{@github_url}`
-      say 'cloning complete'
-    end
+    # if not File.exists?(current_path + '/ev3nly.github.io/.git')
+    #   speak_and_say 'cloning github pages url'
+    #   `git clone #{@github_url}`
+    #   say 'cloning complete'
+    # end
 
-    speak_and_say 'building distribution files'
-    # `grunt build --force`
-    say 'build complete'
+    # speak_and_say 'building distribution files'
+    # # `grunt build --force`
+    # say 'build complete'
     
-    speak_and_say 'moving distribution files to github.io repo'
+    # speak_and_say 'moving distribution files to github.io repo'
 
-    Dir.glob(File.join(File.expand_path("./dist"), '*')).each do |file|
-      if File.exists?(file)
-        FileUtils.move(file, "ev3nly.github.io/#{File.basename(file)}")
-      end
-    end
+    # Dir.glob(File.join(File.expand_path("./dist"), '*')).each do |file|
+    #   if File.exists?(file)
+    #     FileUtils.move(file, "ev3nly.github.io/#{File.basename(file)}")
+    #   end
+    # end
 
-    speak_and_say 'commiting files'
+    # speak_and_say 'commiting files'
     `git add .`
     `git commit . -m 'Rebuilding dist'`
     `git push`
