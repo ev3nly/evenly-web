b875e2704848995ea133e7246f781d885766c0d0
Sean Yu <seansu4you87@gmail.com>
2013-07-15 12:50:06 -0700

    Fixing nav display on static pages

--- app/scripts/controllers/staticNav.js
+++ app/scripts/controllers/staticNav.js
@@ -1,11 +1,13 @@
 'use strict';
 
 angular.module('evenlyApp')
-  .controller('StaticNavCtrl', ['$scope', '$location', function ($scope, $location) {
+  .controller('StaticNavCtrl', ['$scope', '$location', '$rootScope', function ($scope, $location, $rootScope) {
     $scope.stateForItem = function(item) {
       var currentPath = $location.path().substring(1);
       return (item === currentPath) ? 'active' : '';
     }
 
     $('nav.static-nav').affix();
+
+    $rootScope.showNav = false;
   }]);
