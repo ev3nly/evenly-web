d70d808c3138bfa346b33ab87cbbd033d7dbea02
Sean Yu <seansu4you87@gmail.com>
2013-07-15 16:42:56 -0700

    Profile nav better styled, ActionBar refactored into its own view

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -2,5 +2,12 @@
 
 angular.module('evenlyApp')
   .controller('ProfileCtrl', ['$scope', function ($scope) {
-    
+    $scope.logout = function() {
+      var wantsToLogout = confirm('Are you sure you want to logout?');
+      if (wantsToLogout) {
+        alert('should log you out');
+      } else {
+
+      }
+    };
   }]);
